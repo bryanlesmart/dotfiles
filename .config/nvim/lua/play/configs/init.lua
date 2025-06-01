9d90d2031af0cd86aaec799a5bb3645fc69b3eca
lesmart <bryanlesmart03@gmail.com>
2025-06-01 22:30:53 +0800

    Init dotfiles

---
+++ .config/nvim/lua/play/configs/init.lua
@@ -0,0 +1,14 @@
+require("play.configs.keymaps")
+require("play.configs.options")
+
+
+
+vim.g.mapleader = " "
+
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
---
+++ .config/nvim/lua/play/configs/options.lua
@@ -0,0 +1,32 @@
+
+
+vim.o.mouse = 'a'
+
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+
+-- tabs & indentation
+vim.opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+vim.opt.shiftwidth = 2 -- 2 spaces for indent width
+vim.opt.expandtab = true -- expand tab to spaces
+vim.opt.autoindent = true -- copy indent from current line when starting new one
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+
+vim.opt.relativenumber = true
+
+vim.schedule(function() 
+	vim.o.clipboard = "unnamedplus"
+end)
