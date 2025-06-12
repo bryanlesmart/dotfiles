2b40920a60427c2314f92841b054c8caa3b84efa
Lesmart <bryansilmaro03@gmail.com>
2025-06-12 13:03:04 +0800

    mod:  font

--- .config/nvim/lua/play/plugins/colorscheme.lua
+++ .config/nvim/lua/play/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
 	config = function()
 		vim.opt.termguicolors = true
 		vim.cmd.colorscheme("melange")
-		vim.cmd.hi("Normal guibg=none")
+		vim.cmd.hi("Normal guibg=#000000")
 		vim.cmd.hi("NonText guibg=none")
 		vim.cmd.hi("Normal ctermbg=none")
 		vim.cmd.hi("NonText ctermbg=none")
